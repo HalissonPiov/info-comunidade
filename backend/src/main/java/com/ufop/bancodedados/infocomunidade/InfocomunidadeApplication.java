be80700880fb3faec8c978566af98829e2a5a508
Davi Abner <davi.abner57@gmail.com>
2026-01-02 17:57:24 -0300

    config: adiciona configuração inicial do backend e conecta o banco de dados MongoDB

---
+++ backend/src/main/java/com/ufop/bancodedados/infocomunidade/InfocomunidadeApplication.java
@@ -0,0 +1,13 @@
+package com.ufop.bancodedados.infocomunidade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfocomunidadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfocomunidadeApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ufop/bancodedados/infocomunidade/InfocomunidadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufop.bancodedados.infocomunidade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfocomunidadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
