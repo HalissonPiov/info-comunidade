03727232d57da8a1c7a9d64dc8929bcd29b84bc5
Hálisson Piovezana <halissonpiovezana5@gmail.com>
2026-01-05 12:45:25 -0300

    feat(models): modela entidade Publicacao

---
+++ backend/src/main/java/com/ufop/bancodedados/infocomunidade/models/Publicacao.java
@@ -0,0 +1,26 @@
+package com.ufop.bancodedados.infocomunidade.models;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@Document(collection = "publicacao")
+public abstract class Publicacao implements Serializable {
+
+    @Id
+    private String idPublicacao;
+
+    private String titulo;
+    private String descricao;
+    private LocalDateTime dataCriacao;
+    private List<String> hashtags;
+    private Endereco endereco;
+    private String imagemURL;
+//    private Usuario usuario;
+
+}
