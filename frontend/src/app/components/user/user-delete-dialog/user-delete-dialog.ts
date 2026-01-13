349bcb96584badf970897f4c3f56394d872877d6
Maria Clara Barbosa Fernandes <bmariaclara831@gmail.com>
2026-01-13 18:06:38 -0300

    feat: adiciona deleção de conta

---
+++ frontend/src/app/components/user/user-delete-dialog/user-delete-dialog.ts
@@ -0,0 +1,54 @@
+import { Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MAT_DIALOG_DATA, MatDialogActions, MatDialogClose, MatDialogContent, MatDialogRef } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+import { UserService } from '../../../services/user-service';
+import { AuthUserService } from '../../../services/auth-user-service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-user-delete-dialog',
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatDialogContent,
+    MatDialogActions,
+    MatDialogClose,
+  ],
+  templateUrl: './user-delete-dialog.html',
+  styleUrl: './user-delete-dialog.css',
+})
+export class UserDeleteDialog {
+  readonly dialogRef = inject(MatDialogRef<UserDeleteDialog>);
+  readonly data = inject<any>(MAT_DIALOG_DATA);
+  private router = inject(Router);
+  private authUserService: AuthUserService = inject(AuthUserService)
+
+  constructor(private userService: UserService) {
+  }
+
+  closeDialog() {
+    this.dialogRef.close();
+  }
+
+  deleteUser() {
+    const id = this.authUserService.getUserFromStorage()?.id;
+    if (!id) return;
+
+    this.userService.deleteUser(id)
+    .subscribe(
+        (response) => {
+          this.router.navigate(['/login']);
+          this.authUserService.logout()
+          this.dialogRef.close(true);
+
+        },
+        (err) => {
+          console.log('Erro ao atualizar perfil', err);
+        }
+      );
+  }
+}
--- frontend/src/app/components/user/user-form-dialog/user-form-dialog.ts
+++ frontend/src/app/components/user/user-form-dialog/user-form-dialog.ts
@@ -1,10 +1,11 @@
+import { AuthUserService } from './../../../services/auth-user-service';
 import { Component, inject } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
+import { User } from '../../../models/User';
+import { UserService } from '../../../services/user-service';
 import { SharedModule } from '../../../shared/shared-module';
-import { User } from '../model/User';
-import { UserService } from '../service/user-service';
 
 @Component({
   selector: 'app-user-form-dialog',
@@ -19,12 +20,14 @@ export class UserFormDialog {
   );
 
   private formBuilder = inject(FormBuilder);
+  private authUserService: AuthUserService = inject(AuthUserService)
 
   public userForm = this.formBuilder.group({
     name: ['', Validators.required],
     username: ['', Validators.required],
     bairro: [''],
     dataNascimento: [''],
+    senha: ['', Validators.required],
   });
 
   constructor(private userService: UserService) {
@@ -41,7 +44,7 @@ export class UserFormDialog {
   }
 
   onUpdateSubmit() {
-    const id = this.userService.getUserInLocalStorage().id;
+    const id = this.authUserService.getUserFromStorage()?.id;
     if (!id) return;
 
     const user: User = {
@@ -49,16 +52,17 @@ export class UserFormDialog {
       username: this.userForm.value.username as string,
       bairro: this.userForm.value.bairro as string,
       dataNascimento: this.userForm.value.dataNascimento as string,
+      senha: this.userForm.value.senha as string,
     };
 
     this.userService
       .updateUserData(id, user)
       .subscribe(
         (response) => {
-          console.log("deu bom")
+          this.dialogRef.close(true);
         },
         (err) => {
-          console.log('Erro ao listar guia', err);
+          console.log('Erro ao atualizar perfil', err);
         }
       );
   }
