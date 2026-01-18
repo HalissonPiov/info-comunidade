960535d3b1936e95aaebfca5026cc7efe23af603
Hálisson Piovezana <halissonpiovezana5@gmail.com>
2026-01-17 23:49:47 -0300

    feat: implementa camada controller de endereco com endpoints de atualizar, buscar por id, por rua e por bairro

---
+++ backend/src/main/java/com/ufop/bancodedados/infocomunidade/controllers/EnderecoController.java
@@ -0,0 +1,51 @@
+package com.ufop.bancodedados.infocomunidade.controllers;
+
+import com.ufop.bancodedados.infocomunidade.models.Endereco;
+import com.ufop.bancodedados.infocomunidade.services.EnderecoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+
+@Controller
+@RequestMapping("endereco")
+public class EnderecoController {
+
+    @Autowired
+    public EnderecoService enderecoService;
+
+    @PutMapping("{id}")
+    public ResponseEntity<Void> atualizar(@PathVariable String id, @RequestBody Endereco endereco){
+        enderecoService.atualizar(id, endereco);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Endereco> buscarPorId(@PathVariable String id){
+        Endereco enderecoId = enderecoService.buscarPorId(id);
+        if(enderecoId == null){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(enderecoId);
+    }
+
+    @GetMapping("/rua")
+    public ResponseEntity<Endereco> buscarPorRua(@PathVariable String rua){
+        Endereco EnderecoRua = enderecoService.buscarPorRua(rua);
+        if(EnderecoRua == null){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(EnderecoRua);
+    }
+
+    @GetMapping("/bairro")
+    public ResponseEntity<Endereco> buscarPorBairro(@PathVariable String bairro){
+        Endereco enderecoBairro = enderecoService.buscarPorBairro(bairro);
+        if(enderecoBairro == null){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(enderecoBairro);
+    }
+
+}
