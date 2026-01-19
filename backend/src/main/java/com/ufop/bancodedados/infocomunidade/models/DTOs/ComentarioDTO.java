2fdeb73bc579b1bd9fbb2c12a5d2d430d63d3bbc
Davi Abner <davi.abner57@gmail.com>
2026-01-19 09:55:56 -0300

    feat: implementa restrições de conteúdo para texto no DTO de Comentário

--- backend/src/main/java/com/ufop/bancodedados/infocomunidade/models/DTOs/ComentarioDTO.java
+++ backend/src/main/java/com/ufop/bancodedados/infocomunidade/models/DTOs/ComentarioDTO.java
@@ -1,5 +1,7 @@
 package com.ufop.bancodedados.infocomunidade.models.DTOs;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,8 +10,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ComentarioDTO {
+    @NotBlank(message = "Para publicar um comentário, o texto do mesmo é obrigatório")
+    @Size(min = 1, max = 350, message = "O comentário deve ter entre 1 e 350 caracteres")
     private String texto;
     private String publicacaoID;
     private String usuarioID;
     private boolean excluido;
 }
+
